d73339abf17fa5535d3b38b0ece7a36dd8d9f413
egorzkie <67702227+egorzkie@users.noreply.github.com>
2023-09-17 04:33:11 -0400

    Hardcoded choose pages

--- feyn/react/src/Components/Pages/Pages.js
+++ feyn/react/src/Components/Pages/Pages.js
@@ -1,24 +1,45 @@
+// demo 
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import Header from "../Header/Header.js";
 import "./Pages.css"
 import "../index.css"
-import React from 'react';
-import { Link } from "react-router-dom";
-import Header from "../Header/Header.js"
-import { Document, Page } from 'react-pdf';
 
 const Pages = () => {
+    const [selectedPages, setSelectedPages] = useState([]);
+    const images = ['../../../images/x.png', '../../../images/x.png', '../../../images/x.png', '../../../images/x.png', '../../../images/x.png']; // Add paths of your images here
+
+    const handleImageClick = (index) => {
+        if (selectedPages.includes(index)) {
+            setSelectedPages(prev => prev.filter(i => i !== index));
+        } else {
+            setSelectedPages(prev => [...prev, index]);
+        }
+    };
 
     return (
         <div>
             <Header />
-            <div class="center-container">
+            <div className="center-container">
                 <h1>Choose the pages you want to teach</h1>
-                <Link to="/Record" class="main-button">Next</Link>
+
+                <div className="image-grid">
+                    {images.map((img, index) => (
+                        <img 
+                            key={index} 
+                            src={img} 
+                            alt={`Page ${index + 1}`} 
+                            onClick={() => handleImageClick(index)}
+                            className={selectedPages.includes(index) ? 'selected' : ''}
+                        />
+                    ))}
+                </div>
+                <br/><br/><br/><br/>
+                <Link to="/Record" className="main-button">Next</Link>
             </div>
-
-            <button class="back-button">&lt;</button>
+            <button className="back-button">&lt;</button>
         </div>
-    )
-
+    );
 };
 
 export default Pages;
